3e597de45de25bab273cf4e165d7aba4c6312055
javiersa <javiersa@student.42malaga.com>
2023-04-13 20:21:27 +0200

    Little changes

--- Plus/ft_freeandjoin.c
+++ Plus/ft_freeandjoin.c
@@ -6,7 +6,7 @@
 /*   By: javiersa <javiersa@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 19:00:01 by javiersa          #+#    #+#             */
-/*   Updated: 2023/03/29 17:53:02 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/04/13 20:20:27 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ char	*ft_freeandjoin(char *buffer, char *aux)
 	char	*strjoin;
 
 	strjoin = ft_strjoin(buffer, aux);
-	ft_free_and_null((void **) &buffer);
+	ft_multiple_free(2, buffer, aux);
 	return (strjoin);
 }
