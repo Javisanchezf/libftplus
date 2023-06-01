04ac6dedd5c56195bd5dfa5ec0b258349a594dd2
javiersa <javiersa@student.42malaga.com>
2023-06-01 17:57:47 +0200

    Little changes 2023-06-01 17:57:47

--- Plus/ft_split_free.c
+++ Plus/ft_split_free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split_free.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: javiersa <javiersa@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: javiersa <javiersa@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 17:27:03 by javiersa          #+#    #+#             */
-/*   Updated: 2023/05/18 17:45:30 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/06/01 17:57:35 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ void	ft_split_free(char **str)
 	ptr = str;
 	while (*ptr != NULL)
 	{
-		if (**ptr != '\0')
-			ft_free_and_null((void **)&*ptr);
+		ft_free_and_null((void **)&*ptr);
 		ptr++;
 	}
 	ft_free_and_null((void **)&str);
